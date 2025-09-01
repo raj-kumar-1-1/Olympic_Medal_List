9cf6968890a4fe89e652b98be734705f058d4f43
rajkumar8853 <22h51a05e8@cmrcet.ac.in>
2025-09-01 14:12:56 +0530

    edit

---
+++ backend/assets/html/Main.java
@@ -0,0 +1,12 @@
+class Main{
+    public static  void main(String[] args) {
+        {
+            x=7;
+            y+=x;
+            System.out.println(x+" "+y);
+            int x;
+            int y;
+
+        }
+    }
+}
